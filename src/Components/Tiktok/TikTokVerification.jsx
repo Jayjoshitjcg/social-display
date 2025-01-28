a91f6bfbc9442cac5392d3ae2c5f6565b4085e36
Jayjoshitjcg <jayjoshi@tjcg.in>
2025-01-28 17:16:20 +0530

    update tiktok

--- src/Components/Tiktok/TikTokVerification.jsx
+++ src/Components/Tiktok/TikTokVerification.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TikTokVerification = () => {
     // Content of your TikTok verification file
-    const verificationContent = "PB7jVKov7d2py3Jm0e8QWXUFotzPwgIk";
+    const verificationContent = "tiktok-developers-site-verification=PB7jVKov7d2py3Jm0e8QWXUFotzPwgIk";
 
     // Render the content as plain text
     return (
